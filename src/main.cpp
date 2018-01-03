b404646fa04511496080c8d34e9011b87238361e
larso0 <lars.olav.tolo@gmail.com>
2018-01-03 17:13:53 +0100

    Enumerating device groups

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
-int main()
+#include <bp/Instance.h>
+#include <bp/Device.h>
+#include <stdexcept>
+
+using namespace bp;
+using namespace std;
+
+int main(int argc, char** argv)
 {
-	std::cout << "Hello, World!" << std::endl;
+	Instance instance;
+	instance.enableExtension("VK_KHX_device_group_creation");
+	instance.init(true);
+
+	DeviceRequirements requirements{};
+	requirements.extensions.emplace_back("VK_KHX_device_group");
+	requirements.queues = VK_QUEUE_GRAPHICS_BIT | VK_QUEUE_TRANSFER_BIT;
+
+	auto result = queryDevices(instance, requirements);
+
+	if (result.empty())
+		throw runtime_error("No suitable devices.");
+
+	PFN_vkEnumeratePhysicalDeviceGroupsKHX vkEnumeratePhysicalDeviceGroupsKHX =
+		reinterpret_cast<PFN_vkEnumeratePhysicalDeviceGroupsKHX>(
+			vkGetInstanceProcAddr(instance, "vkEnumeratePhysicalDeviceGroupsKHX"));
+
+	uint32_t n;
+	vkEnumeratePhysicalDeviceGroupsKHX(instance, &n, nullptr);
+	vector<VkPhysicalDeviceGroupPropertiesKHX> groupProperties(n);
+	vkEnumeratePhysicalDeviceGroupsKHX(instance, &n, groupProperties.data());
+
+	for (auto& group : groupProperties)
+	{
+		cout << "Group with " << group.physicalDeviceCount << " devices" << endl;
+	}
+
 	return 0;
 }
